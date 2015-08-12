50234a8ec510ad779df8ba2ad2936efbe5342f8d
André decko de Brito <decko@birosca.mobi>
2015-08-12 18:38:24 -0300

    Define a entidade Pedido

--- ceac/analise/models.py
+++ ceac/analise/models.py
@@ -3,18 +3,34 @@ from django.db import models
 
 from django.utils.translation import ugettext_lazy as _
 
+from django_postgres import JSONField
 
-class Analise(models.Model):
+
+class Pedido(models.Model):
     ente = models.ForeignKey('core.Ente')
+    data_pedido = models.DateField(_('Data do Pedido'))
+    areas_atuacao = JSONField(_('Áreas de Atuação'))
+
+    class Meta:
+        verbose_name = "pedido"
+        verbose_name_plural = "pedidos"
+
+    def get_absolute_url():
+        return reverse('pedido.views.details', args=[str(self.id)])
+
+
+class Parecer(models.Model):
+    pedido = models.ForeignKey(Pedido)
     responsavel = models.ForeignKey(settings.AUTH_USER_MODEL, null=True, blank=True)
     texto_parecer = models.TextField(_('Parecer'))
     decisao = models.IntegerField(_('Decisão do parecerista'), max_length=1, choices=DECISAO_ANALISE)
     data_decisao = models.DateField(_('Data da Decisão'), auto_add=True)
+    areas_atuação = JSONField(_('Áreas de Atuação'))
 
     class Meta:
         verbose_name = "analise"
         verbose_name_plural = "analises"
 
     def get_absolute_url(self):
-        return reverse('analise.views.details', args=[str(self.id)])
+        return reverse('parecer.views.details', args=[str(self.id)])
 
